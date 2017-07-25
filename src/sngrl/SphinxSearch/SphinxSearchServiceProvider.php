655694839fb46219070d5f2d3a59163eef91195a
kamikax <kamikax@gmail.com>
2017-07-25 22:09:19 +0900

    change for laravel 5.4

--- src/sngrl/SphinxSearch/SphinxSearchServiceProvider.php
+++ src/sngrl/SphinxSearch/SphinxSearchServiceProvider.php
@@ -1,27 +1,31 @@
-<?php 
+<?php
 namespace sngrl\SphinxSearch;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class SphinxSearchServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
     public function register()
     {
-        $this->app['sphinxsearch'] = $this->app->share(function ($app) {
+        $this->app->singleton(SphinxSearch::class, function ($app) {
             return new SphinxSearch;
         });
     }
-
-
     public function boot()
     {
         $this->publishes([
             ## Original
-            #__DIR__.'../../../../config/sphinxsearch.php' => config_path('sphinxsearch.php'),
-
+            //__DIR__.'../../../../config/sphinxsearch.php' => config_path('sphinxsearch.php'),
             ## https://github.com/sngrl/sphinxsearch/issues/3
             __DIR__.'/../../../config/sphinxsearch.php' => config_path('sphinxsearch.php'),
         ]);
     }
-
 }
