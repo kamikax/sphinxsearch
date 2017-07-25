8fdf33e24d2d1c471ebab3bcee74fa06327c78ff
kamikax <kamikax@gmail.com>
2017-07-25 22:08:18 +0900

    overwrite

--- src/sngrl/SphinxSearch/SphinxSearch.php
+++ src/sngrl/SphinxSearch/SphinxSearch.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace sngrl\SphinxSearch;
 
 class SphinxSearch
@@ -10,8 +10,7 @@ class SphinxSearch
     protected $_total_count;
     protected $_time;
     protected $_eager_loads;
-	protected $_raw_mysql_connection;
-
+    protected $_raw_mysql_connection;
     public function __construct()
     {
         $host = \Config::get('sphinxsearch.host');
@@ -30,55 +29,51 @@ class SphinxSearch
         $this->_index_name = isset($this->_config['name']) ? implode(',', $this->_config['name']) : key($this->_config);
         $this->_eager_loads = array();
     }
-
-	/**
-	 * @param $docs
-	 * @param $index_name
-	 * @param $query
-	 * @param array $extra, in this format: array('option_name' => option_value, 'limit' => 100, ...)
-	 * @return array
-	 */
-	public function getSnippetsQL($docs, $index_name, $query, $extra = [])
-	{
-		// $extra = [];
-		if (is_array($docs) === FALSE)
-		{
-			$docs = [$docs];
-		}
-		foreach ($docs as &$doc)
-		{
-			$doc = "'".mysqli_real_escape_string($this->_raw_mysql_connection, strip_tags($doc))."'";
-		}
-
-		$extra_ql = '';
-		if ($extra)
-		{
-			foreach ($extra as $key => $value)
-			{
-				$extra_ql[] = $value.' AS '.$key;
-			}
-			$extra_ql = implode(',', $extra_ql);
-			if ($extra_ql)
-			{
-				$extra_ql = ','.$extra_ql;
-			}
-		}
-
-		$query = "CALL SNIPPETS((".implode(',',$docs)."),'".$index_name."','".mysqli_real_escape_string($this->_raw_mysql_connection, $query)."' ".$extra_ql.")";
-		// die($query);
-		$result = mysqli_query($this->_raw_mysql_connection, $query);
-		// ddd($result);
-		$reply = array();
-		if ($result)
-		{
-			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-			{
-				$reply[] = $row['snippet'];
-			}
-		}
-		return $reply;
-	}
-
+    /**
+     * @param $docs
+     * @param $index_name
+     * @param $query
+     * @param array $extra, in this format: array('option_name' => option_value, 'limit' => 100, ...)
+     * @return array
+     */
+    public function getSnippetsQL($docs, $index_name, $query, $extra = [])
+    {
+        // $extra = [];
+        if (is_array($docs) === FALSE)
+        {
+            $docs = [$docs];
+        }
+        foreach ($docs as &$doc)
+        {
+            $doc = "'".mysqli_real_escape_string($this->_raw_mysql_connection, strip_tags($doc))."'";
+        }
+        $extra_ql = '';
+        if ($extra)
+        {
+            foreach ($extra as $key => $value)
+            {
+                $extra_ql[] = $value.' AS '.$key;
+            }
+            $extra_ql = implode(',', $extra_ql);
+            if ($extra_ql)
+            {
+                $extra_ql = ','.$extra_ql;
+            }
+        }
+        $query = "CALL SNIPPETS((".implode(',',$docs)."),'".$index_name."','".mysqli_real_escape_string($this->_raw_mysql_connection, $query)."' ".$extra_ql.")";
+        // die($query);
+        $result = mysqli_query($this->_raw_mysql_connection, $query);
+        // ddd($result);
+        $reply = array();
+        if ($result)
+        {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+            {
+                $reply[] = $row['snippet'];
+            }
+        }
+        return $reply;
+    }
     public function search($string, $index_name = null)
     {
         $this->_search_string = $string;
@@ -95,61 +90,56 @@ class SphinxSearch
         $this->_connection->resetGroupBy();
         return $this;
     }
-
     public function setFieldWeights($weights)
     {
         $this->_connection->setFieldWeights($weights);
         return $this;
     }
-
     public function setMatchMode($mode)
     {
         $this->_connection->setMatchMode($mode);
         return $this;
     }
-
     public function setRankingMode($mode)
     {
         $this->_connection->setRankingMode($mode);
         return $this;
     }
-
     public function setSortMode($mode, $sortby = null)
     {
         $this->_connection->setSortMode($mode, $sortby);
         return $this;
     }
-
     public function setFilterFloatRange($attribute, $min, $max, $exclude = false)
     {
         $this->_connection->setFilterFloatRange($attribute, $min, $max, $exclude);
         return $this;
     }
-
+    public function setFilterString($attribute, $value, $exclude = false)
+    {
+        $this->_connection->setFilterString($attribute, $value, $exclude);
+        return $this;
+    }
     public function setGeoAnchor($attrlat, $attrlong, $lat = null, $long = null)
     {
         $this->_connection->setGeoAnchor($attrlat, $attrlong, $lat, $long);
         return $this;
     }
-
     public function setGroupBy($attribute, $func, $groupsort = '@group desc')
     {
         $this->_connection->setGroupBy($attribute, $func, $groupsort);
         return $this;
     }
-
     public function setSelect($select)
     {
         $this->_connection->setSelect($select);
         return $this;
     }
-
     public function limit($limit, $offset = 0, $max_matches = 1000, $cutoff = 1000)
     {
         $this->_connection->setLimits($offset, $limit, $max_matches, $cutoff);
         return $this;
     }
-
     public function filter($attribute, $values, $exclude = false)
     {
         if (is_array($values)) {
@@ -163,28 +153,23 @@ class SphinxSearch
         $this->_connection->setFilter($attribute, $val, $exclude);
         return $this;
     }
-
     public function range($attribute, $min, $max, $exclude = false)
     {
         $this->_connection->setFilterRange($attribute, $min, $max, $exclude);
         return $this;
     }
-
     public function query()
     {
         return $this->_connection->query($this->_search_string, $this->_index_name);
     }
-
     public function excerpt($content, $opts = array())
     {
         return $this->_connection->buildExcerpts(array($content), $this->_index_name, $this->_search_string, $opts);
     }
-
     public function excerpts($contents, $opts = array())
     {
         return $this->_connection->buildExcerpts($contents, $this->_index_name, $this->_search_string, $opts);
     }
-
     public function get($respect_sort_order = false)
     {
         $this->_total_count = 0;
@@ -237,7 +222,6 @@ class SphinxSearch
         $this->_eager_loads = array();
         return $result;
     }
-
     public function with()
     {
         // Allow multiple with-calls
@@ -254,22 +238,18 @@ class SphinxSearch
         }
         return $this;
     }
-
     public function getTotalCount()
     {
         return $this->_total_count;
     }
-
     public function getTime()
     {
         return $this->_time;
     }
-
     public function getErrorMessage()
     {
         return $this->_connection->getLastError();
     }
-
     private function getResultKeyByID($id, $result)
     {
         if (count($result) > 0) {
@@ -281,10 +261,8 @@ class SphinxSearch
         }
         return false;
     }
-
     public function escapeStringQL($string)
     {
         return $this->_connection->escapeString($string);
     }
-
 }
